d4b1652e5543f9d7dddc673edcf19e4ee606778f
Bastien Tagliaro <37577064+btglr@users.noreply.github.com>
2020-11-09 19:23:12 +0100

    Create function apply_non_local_means
    Converts a DICOM image into an OpenCV-readable format and
    applies a fast non-local means denoising

--- helpers.py
+++ helpers.py
@@ -1,6 +1,10 @@
 from pathlib import Path
+
+import cv2
+import numpy as np
 from pydicom import dcmread
 from scipy import ndimage
+from skimage import exposure
 
 
 def load_dataset(path):
@@ -13,3 +17,13 @@ def apply_denoise(img, denoise_filter=ndimage.median_filter, kernel_size=3):
     new_img = denoise_filter(img.pixel_array, kernel_size)
 
     return new_img
+
+
+def apply_non_local_means(img, strength=10, kernel=7, window_search=21):
+    dcm_sample = img.pixel_array * 128
+    dcm_sample = exposure.equalize_adapthist(dcm_sample)
+    dcm_sample *= 255
+
+    img_uint8 = np.uint8(dcm_sample)
+
+    return cv2.fastNlMeansDenoising(img_uint8, None, strength, kernel, window_search)
