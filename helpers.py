a55f88436ab39cd47cdf38f588720f0e4aabb780
Bastien Tagliaro <37577064+btglr@users.noreply.github.com>
2020-11-16 17:43:54 +0100

    Retain original data type after noise filtering

--- helpers.py
+++ helpers.py
@@ -20,13 +20,18 @@ def apply_simple_denoise(img, denoise_filter=ndimage.median_filter, kernel_size=
 
 
 def apply_non_local_means(img, strength=10, kernel=7, window_search=21):
+    # Retain original data type
+    orig_dtype = img.pixel_array.dtype
+
     dcm_sample = img.pixel_array * 128
     dcm_sample = exposure.equalize_adapthist(dcm_sample)
     dcm_sample *= 255
 
     img_uint8 = np.uint8(dcm_sample)
 
-    return cv2.fastNlMeansDenoising(img_uint8, None, strength, kernel, window_search)
+    new_img = cv2.fastNlMeansDenoising(img_uint8, None, strength, kernel, window_search)
+
+    return new_img.astype(orig_dtype)
 
 
 # d: Diameter of each pixel neighborhood.
@@ -37,10 +42,15 @@ def apply_non_local_means(img, strength=10, kernel=7, window_search=21):
 # The greater its value, the more further pixels will mix together,
 # given that their colors lie within the sigmaColor range.
 def apply_bilateral_filtering(img, d=15, sigmacolor=75, sigmacoordinate=75):
+    # Retain original data type
+    orig_dtype = img.pixel_array.dtype
+
     dcm_sample = img.pixel_array * 128
     dcm_sample = exposure.equalize_adapthist(dcm_sample)
     dcm_sample *= 255
 
     img_uint8 = np.uint8(dcm_sample)
 
-    return cv2.bilateralFilter(img_uint8, d, sigmacolor, sigmacoordinate)
+    new_img = cv2.bilateralFilter(img_uint8, d, sigmacolor, sigmacoordinate)
+
+    return new_img.astype(orig_dtype)
