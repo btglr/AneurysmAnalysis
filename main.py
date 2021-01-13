0aa7fbba2110627f57e137ca851ea75d2dda7728
Bastien Tagliaro <37577064+btglr@users.noreply.github.com>
2021-01-13 13:58:15 +0100

    Remove unused debug messages

--- main.py
+++ main.py
@@ -7,42 +7,14 @@ ds = load_dataset(dataset_path)
 
 # Iterate through the PATIENT records
 for patient in ds.patient_records:
-    # print(
-    #     f"PATIENT: PatientID={patient.PatientID}, "
-    #     f"PatientName={patient.PatientName}"
-    # )
-
     # Find all the STUDY records for the patient
-    studies = [
-        ii for ii in patient.children if ii.DirectoryRecordType == "STUDY"
-    ]
+    studies = [ii for ii in patient.children if ii.DirectoryRecordType == "STUDY"]
     for study in studies:
-        descr = study.StudyDescription or "(no value available)"
-        # print(
-        #     f"{'  ' * 1}STUDY: StudyID={study.StudyID}, "
-        #     f"StudyDate={study.StudyDate}, StudyDescription={descr}"
-        # )
-
         # Find all the SERIES records in the study
-        all_series = [
-            ii for ii in study.children if ii.DirectoryRecordType == "SERIES"
-        ]
+        all_series = [ii for ii in study.children if ii.DirectoryRecordType == "SERIES"]
         for series in all_series:
             # Find all the IMAGE records in the series
-            images = [
-                ii for ii in series.children
-                if ii.DirectoryRecordType == "IMAGE"
-            ]
-            plural = ('', 's')[len(images) > 1]
-
-            descr = getattr(
-                series, "SeriesDescription", "(no value available)"
-            )
-            # print(
-            #     f"{'  ' * 2}SERIES: SeriesNumber={series.SeriesNumber}, "
-            #     f"Modality={series.Modality}, SeriesDescription={descr} - "
-            #     f"{len(images)} SOP Instance{plural}"
-            # )
+            images = [ii for ii in series.children if ii.DirectoryRecordType == "IMAGE"]
 
             # Get the absolute file path to each instance
             #   Each IMAGE contains a relative file path to the root directory
@@ -57,8 +29,6 @@ for patient in ds.patient_records:
             i = 0
             # List the instance file paths
             for p in paths:
-                # print(f"{'  ' * 3}IMAGE: Path={os.fspath(p)}")
-
                 img = dcmread(Path(dataset_path).joinpath(p))
                 images.append((img, p))
 
